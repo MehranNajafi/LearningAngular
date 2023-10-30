6060ae53d94142bb6c3dd16926fbb0a1dcc7d74a
MehranNajafi <mehran.snape@gmail.com>
2023-10-30 20:40:56 +0330

    map_references

--- src/app/request/request.component.ts
+++ src/app/request/request.component.ts
@@ -1,32 +1,46 @@
-import { AfterViewInit, Component, Inject, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, Inject, ViewChild, Pipe } from '@angular/core';
 import { FollowersService } from '../services/followers.service';
 import { ActivatedRoute } from '@angular/router';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatSort } from '@angular/material/sort';
+import { ProjectService } from '../project/project.service';
+import { combineLatest, map } from 'rxjs';
 
 @Component({
   selector: 'request',
   templateUrl: './request.component.html',
-  styleUrls: ['./request.component.css']
+  styleUrls: ['./request.component.css'],
 })
 export class RequestComponent {
-  constructor(private service: FollowersService) { }
-  requests: IRequest[];
-  displayedColumns: string[] = ['id', 'subject', 'kind', 'team', 'project', 'status'];
+  constructor(
+    private projectService: ProjectService,
+    private service: FollowersService
+  ) {}
+  displayedColumns: string[] = [
+    'id',
+    'subject',
+    'kind',
+    'team',
+    'project',
+    'status',
+  ];
   dataSource!: MatTableDataSource<IRequest>;
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
+
+  requests = this.service.requests$;
+  projects = this.projectService.projects$;
+  projectMap: { [id: string]: string } = {};
+
   ngOnInit() {
-    this.service.getAllFake().subscribe((reqs) => {
-      this.requests = reqs.requests;
-      this.dataSource = new MatTableDataSource<IRequest>(
-        this.requests
-      );
-      this.dataSource.sort = this.sort;
-      this.dataSource.paginator = this.paginator;
+    this.projects.forEach(project => {
+      this.projectMap[project.id] = project.subject;
     });
-  };
+    this.dataSource = new MatTableDataSource<IRequest>(this.requests);
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
 
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
@@ -38,10 +52,10 @@ export class RequestComponent {
   }
 }
 export interface IRequest {
-  id: number,
-  subject: string,
-  kind: number,
-  team: number,
-  project: number,
-  status: number
+  id: number;
+  subject: string;
+  kind: number;
+  team: number;
+  project: number;
+  status: number;
 }
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,10 +1,15 @@
-import { HttpClient, HttpErrorResponse, HttpHeaders, HttpResponse } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpHeaders,
+  HttpResponse,
+} from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, catchError, of, throwError } from 'rxjs';
 import { IRequest } from '../request/request.component';
 
 export class DataService {
-  constructor(private url: string, private http: HttpClient) { }
+  constructor(private url: string, private http: HttpClient) {}
   private httpOptions = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json',
@@ -16,21 +21,18 @@ export class DataService {
   getAll<T>() {
     return this.http.get<T>(this.url).pipe(catchError(this.handleError));
   }
-  getAllFake() {
-    // Simulate your desired response data
-    const requests: IRequest[] = [{ id: 1, subject: 'تست 1', kind: 1, team: 1, project: 1, status: 1 },
+  requests$ = [
+    { id: 1, subject: 'تست 1', kind: 1, team: 1, project: 1, status: 1 },
     { id: 2, subject: 'تست 2', kind: 1, team: 1, project: 2, status: 2 },
     { id: 3, subject: 'تست 3', kind: 2, team: 2, project: 3, status: 3 },
     { id: 4, subject: 'تست 4', kind: 2, team: 3, project: 4, status: 1 },
     { id: 5, subject: 'تست 5', kind: 1, team: 2, project: 1, status: 2 },
-    { id: 6, subject: 'تست 6', kind: 2, team: 3, project: 4, status: 5 },];
-    const fakeResponse = {
-      requests
-    };
-    return of(fakeResponse);
-  }
+    { id: 6, subject: 'تست 6', kind: 2, team: 3, project: 4, status: 5 },
+  ];
   create(post: HTMLInputElement) {
-    return this.http.post(this.url, JSON.stringify(post), this.httpOptions).pipe(catchError(this.handleError));;
+    return this.http
+      .post(this.url, JSON.stringify(post), this.httpOptions)
+      .pipe(catchError(this.handleError));
   }
   update(resource: any) {
     return this.http.patch(
